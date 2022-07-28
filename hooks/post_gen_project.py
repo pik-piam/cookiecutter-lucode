5f58e96dd45869341dffdd789cc07690c0c5c495
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2022-07-28 16:59:31 +0200

    lucode2::buildLibrary refuses to run without an empty R/ directory.

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,7 @@
+"""Generate empty R/ directory. lucode2 needs it to run, but it can't be
+represented in the cookiecutter git repo because git doesn't know empty directories."""
+
+import pathlib
+
+
+pathlib.Path("R").mkdir()
